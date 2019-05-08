fe65bd972435958a827277ebdacb35f10f0d32b9
mkritzl <bitbucket@martin.kritzl.at>
2019-05-08 12:25:45 +0200

    final setup config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 systemctl enable ssh
 apt-get update
-apt-get	install 
+apt-get	install scons python-numpy python-scipy python-pyaudio swig jackd2
+pip3 install rpi_ws281x
 
 
 # http://anleitung.joy-it.net/wp-content/uploads/2017/04/RB-TFT3.2_RB-TFT3.5_Anleitung_09-01-2019-1.pdf
@@ -8,7 +9,7 @@ apt-get	install
 dtparam=spi=on
 dtoverlay=joy-IT-Display-Driver-32b-overlay:rotate=270,swapxy=1
 
-/boot/cmdline.tx
+/boot/cmdline.txt
 fbcon=map:10
 
 /usr/share/X11/xorg.conf.d/99-calibration.conf
@@ -28,3 +29,12 @@ cp joy-IT-Display-Driver-32b-overlay.dtb /boot/overlays/joy-IT-Display-Driver-32
 
 apt-get install xserver-xorg-input-evdev
 cp -rf /usr/share/X11/xorg.conf.d/10-evdev.conf /usr/share/X11/xorg.conf.d/45-evdev.conf
+
+
+
+#https://tutorials-raspberrypi.com/connect-control-raspberry-pi-ws2812-rgb-led-strips/
+sudo nano /etc/modprobe.d/snd-blacklist.conf
+blacklist snd_bcm2835
+
+sudo nano /boot/config.txt
+#dtparam=audio=on
